9987a20d306a275585be2e0bc9a9053ec7067802
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2023-02-04 21:11:02 +0500

    4. Create App Title

---
+++ app/components/Header.tsx
@@ -0,0 +1,14 @@
+// App title
+
+`use client`;
+
+import React from 'react';
+import { Box, Heading } from '@chakra-ui/react';
+
+export default function Header() {
+  return (
+    <Box color='#F1F3F2' textAlign='center' py='5'>
+      <Heading fontSize={['3xl', '4xl']}>Todo&apos;s List</Heading>
+    </Box>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+// Todo App
+
 'use client';
 
 import { Box } from '@chakra-ui/react';
+import Header from './components/Header';
 
+// App Home Page
 export default function Home() {
   return (
     <Box bg='#1D2521' minH='100vh' minW='100vw'>
-      Todo App
+      <Header />
     </Box>
   );
 }
