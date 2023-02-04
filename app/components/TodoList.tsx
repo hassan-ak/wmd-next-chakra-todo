66c6395cf52e94fa3b1542f2feaaed6cb75723ce
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2023-02-04 21:20:16 +0500

    6. Create function to add todo

--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -3,6 +3,7 @@
 // Todos List
 
 import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import { Todo } from '../interface/interface';
 
 export default function TodoList() {
@@ -11,7 +12,6 @@ export default function TodoList() {
   const [title, setTitle] = useState<string>('');
   const [todos, setTodos] = useState<Todo[]>([]);
   const [upTitle, setUpTitle] = useState<string>('');
-  return <div>TodoList</div>;
 
   /************************************************/
   // handle change in input cell
@@ -21,4 +21,24 @@ export default function TodoList() {
   const handleUpdateChange = (e: {
     target: { value: React.SetStateAction<string> };
   }) => setUpTitle(e.target.value);
+
+  // Function to add todo in the list
+  // gets title as an argument
+  // add todo to already listed todos
+  // set title to empty string
+  const addTodo = (title: string) => {
+    if (title != '') {
+      let newTodo: Todo = {
+        id: uuidv4(),
+        title,
+        isDone: false,
+      };
+      const newTodos: Todo[] = [...todos, newTodo];
+      setTodos(newTodos);
+      setTitle('');
+    }
+  };
+
+  /************************************************/
+  return <div>TodoList</div>;
 }
